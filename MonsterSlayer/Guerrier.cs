b15e860f8f2621b5627d47e3023cd3bda138e234
Gabriel <gabriel.luthun@gmail.com>
2024-07-11 13:49:02 +0200

    feat(Guerrier.cs): add Warrior class

---
+++ MonsterSlayer/Guerrier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class Guerrier
+{
+    public string Nom { get; set; }
+    public int PV { get; set; }
+    public int NbDesAttaque { get; set; }
+
+    public Guerrier(string nom, int pv, int nbDesAttaque)
+    {
+        Nom = nom;
+        PV = pv;
+        NbDesAttaque = nbDesAttaque;
+    }
+
+    public virtual void CalculAttaque(Guerrier ennemi, out int attaqueJoueur, out int attaqueEnnemi)
+    {
+        Random desAttaque = new Random();
+        attaqueJoueur = desAttaque.Next(1, 7) * NbDesAttaque;
+        attaqueEnnemi = desAttaque.Next(1, 7) * ennemi.NbDesAttaque;
+    }
+}
+
+
