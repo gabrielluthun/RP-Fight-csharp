05081e3fae2b7d6891be48829a9722068fbb48f5
Gabriel <gabriel.luthun@gmail.com>
2024-07-11 13:48:07 +0200

    feat(Elfe.cs): add Elfe class

---
+++ MonsterSlayer/Elfe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonsterSlayer;
+
+namespace MonsterSlayer
+{
+    internal class Elfe : Guerrier
+    {
+        private int _nbPointsAttaque;
+        public int NbrePointsAttaque { get => _nbPointsAttaque; set => _nbPointsAttaque = value; }
+
+        //Héritage de la classe Guerrier
+        public Elfe(string nom, int pv, int nbDesAttaque, int PointsAttaque) : base(nom, pv, nbDesAttaque)
+        {
+            NbrePointsAttaque = PointsAttaque;
+        }
+
+        public override void CalculAttaque(Guerrier ennemi, out int attaqueJoueur, out int attaqueEnnemi)
+        {
+            Random desAttaque = new Random();
+            attaqueJoueur = desAttaque.Next(1, 7) * NbDesAttaque + NbrePointsAttaque;
+            attaqueEnnemi = desAttaque.Next(1, 7) * ennemi.NbDesAttaque;
+        }
+    }
+}
