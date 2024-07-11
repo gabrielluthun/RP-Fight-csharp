ad01c4f3ff0795b88c893724e95222694c0e6f02
Gabriel <gabriel.luthun@gmail.com>
2024-07-11 13:50:38 +0200

    feat(Nain.cs): add Dwarf class

---
+++ MonsterSlayer/Nain.cs
@@ -0,0 +1,15 @@
+﻿using MonsterSlayer;
+
+namespace MonsterSlayer
+{
+    internal class Nain : Guerrier
+    {
+        private int _pointsDeBouclier;
+        public int Bouclier { get => _pointsDeBouclier; set => _pointsDeBouclier = value; }
+
+        //Héritage de la classe Guerrier
+        public Nain(string nom, int pv, int nbDesAttaque, int pointsDeBouclier) : base(nom, pv, nbDesAttaque) {
+            pointsDeBouclier = Bouclier;
+        }
+    }
+}
